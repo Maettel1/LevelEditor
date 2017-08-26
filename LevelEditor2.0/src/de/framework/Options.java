007aef065a585f94e81353b600c085ce67c1c691
Mättel <Mttel@192.168.178.40>
2017-08-26 20:40:28 +0200

    1

--- LevelEditor2.0/src/de/framework/Options.java
+++ LevelEditor2.0/src/de/framework/Options.java
@@ -3,4 +3,7 @@ package de.framework;
 public class Options {
 	public static boolean animation = false;
 	public static int tileSize = 32;
+	
+	public static int roomWidth = 20;
+	public static int roomHeight = 15;
 }
--- LevelEditor2.0/src/de/framework/Room.java
+++ LevelEditor2.0/src/de/framework/Room.java
@@ -1,5 +1,46 @@
 package de.framework;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.Area;
+import java.util.ArrayList;
+
+import static de.framework.Options.*;
+
 public class Room {
 
+	private ArrayList<Tile> tiles;
+	private Area hitBox;
+	private Dimension dimension;
+	public int x,y;
+	
+	public Room(double x, double y){
+		tiles = new ArrayList<Tile>();
+		
+		this.x = Math.floorDiv((int) x, roomWidth*tileSize)*roomWidth*tileSize;
+		this.y = Math.floorDiv((int) y, roomHeight*tileSize)*roomHeight*tileSize;
+		
+		dimension = new Dimension(roomWidth*tileSize,roomHeight*tileSize);
+		hitBox = new Area(new Rectangle(this.x, this.y, dimension.width, dimension.height));
+	}
+	
+	public void addTile(Tile tile){
+		tiles.add(tile);
+	}
+	
+	public void removeTile(Tile tile){
+		tiles.remove(tile);
+	}
+	
+	public Area getHitBox(){
+		return hitBox;
+	}
+	
+	public void updateDimension(){
+		x = Math.floorDiv((int) x, roomWidth*tileSize)*roomWidth*tileSize;
+		y = Math.floorDiv((int) y, roomHeight*tileSize)*roomHeight*tileSize;
+		
+		dimension = new Dimension(roomWidth*tileSize,roomHeight*tileSize);
+		hitBox = new Area(new Rectangle(this.x, this.y, dimension.width, dimension.height));
+	}
 }
